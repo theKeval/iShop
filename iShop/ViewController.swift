2b1478edd3e305c7b73b81bd1da74b04faeb95ad
Keval <keval.langalia@live.com>
2021-03-16 00:31:57 +0530

    Login page done + new VC added + segue created

--- iShop/ViewController.swift
+++ iShop/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var rbCustomer: UIButton!
+    @IBOutlet weak var rbAdmin: UIButton!
+    @IBOutlet weak var tfUsername: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func customerClicked(_ sender: Any) {
+        rbCustomer.isSelected = true
+        rbAdmin.isSelected = false
+    }
+    
+    @IBAction func admin_clicked(_ sender: Any) {
+        rbAdmin.isSelected = true
+        rbCustomer.isSelected = false
+    }
+    
+    @IBAction func loginClicked(_ sender: Any) {
+    }
+    
 }
 
